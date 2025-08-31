0db110e157d3e7b7ddb37cf1b59fc3a76d0d3c1d
Arkopravob <arka.bhuin@gmail.com>
2025-09-01 05:27:42 +0530

    first commit

---
+++ basics/varibable2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello c++ learner "<<endl;
+    float b = 2.5;
+    cout << b << endl;
+    double d =453.404;
+    cout << d << endl;
+    char c = 'a';
+    cout << c << endl;
+    string s = "hello";
+    cout << s << endl;
+    return 0;
+}
