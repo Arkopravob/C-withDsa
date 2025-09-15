eb55278ed98abd33d9f29562ccf15ac01149f620
Arkopravob <arka.bhuin@gmail.com>
2025-09-15 08:55:29 +0530

    relational operator

---
+++ basics/Operator.cpp
@@ -0,0 +1,22 @@
+//
+// Created by arkap on 15-09-2025.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    //relational operator
+    int a = 2;
+    int b = 3;
+    bool first = (a==b);
+    cout << first << endl;
+    bool second = (a>b);
+    cout << second << endl;
+    bool third = (a<b);
+    cout << third << endl;
+    bool fourth = (a>=b);
+    cout << fourth << endl;
+    bool fifth = (a<=b);
+    cout << fifth << endl;
+    bool sixth = (a!=b);
+    cout << sixth << endl;
+}
