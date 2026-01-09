7e16f2ceb01b6bfa9cee4793afccedde103ef8f7
Arkopravo Bhuin <99447101+Arkopravob@users.noreply.github.com>
2026-01-09 17:27:30 +0530

    variable code

---
+++ variable/variable_demo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+extern  int i , j , k;
+int main()
+{
+    int i = 5 ,j =7 , k = 8;
+    int sum = (i+j+k);
+    cout<<"the sum of three number is :" << sum <<endl;
+
+    float a = 30.9;
+    cout<< "the float value of a is:" << a <<endl;
+    return 0;
+}
